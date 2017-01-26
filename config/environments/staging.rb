ef7ad9e8363cb0893657c63d6d2b22453fe91d82
boleguinp <boleguinp@gmail.com>
2017-01-26 14:46:44 +0000

    more config changes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,5 +4,5 @@ require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Rails.application.routes.default_url_options[:host] = 'https://staging-tcasemgt.herokuapp.com/
+  Rails.application.routes.default_url_options[:host] = 'https://staging-tcasemgt.herokuapp.com/'
 end
